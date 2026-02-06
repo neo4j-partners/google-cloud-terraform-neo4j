1040d529058f1f09f7a7dad58298fc69ae83e83c
Ben Lackey <ben.lackey@outlook.com>
2026-02-05 22:09:24 -0800

    conflate address 2

--- ee/outputs.tf
+++ ee/outputs.tf
@@ -1,9 +1,9 @@
 output "neo4j_http_url" {
   description = "URL to access Neo4j Browser via load balancer"
-  value       = "http://${google_compute_global_address.neo4j_http.address}:7474"
+  value       = "http://${google_compute_global_address.neo4j.address}:7474"
 }
 
 output "neo4j_bolt_endpoint" {
   description = "Bolt endpoint for Neo4j connections via load balancer"
-  value       = "bolt://${google_compute_global_address.neo4j_bolt.address}:7687"
+  value       = "bolt://${google_compute_global_address.neo4j.address}:7687"
 }
