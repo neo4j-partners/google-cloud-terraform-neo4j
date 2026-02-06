ecd07da2d1fec378a80a671a776a87915a6347f1
Ben Lackey <ben.lackey@outlook.com>
2026-02-05 19:55:17 -0800

    drop undesired outputs

--- ee/outputs.tf
+++ ee/outputs.tf
@@ -7,13 +7,3 @@ output "neo4j_bolt_endpoint" {
   description = "Bolt endpoint for Neo4j connections via load balancer"
   value       = "bolt://${google_compute_address.neo4j_bolt.address}:7687"
 }
-
-output "neo4j_mig_name" {
-  description = "Name of the managed instance group"
-  value       = google_compute_instance_group_manager.neo4j.name
-}
-
-output "neo4j_mig_instances" {
-  description = "Instances in the managed instance group"
-  value       = google_compute_instance_group_manager.neo4j.managed_instances
-}
