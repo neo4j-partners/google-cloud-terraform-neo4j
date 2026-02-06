9552464b4b1cd90ce987f0b832948ad5fa9229e5
Ben Lackey <ben.lackey@outlook.com>
2026-02-06 00:21:35 -0800

    simplify

--- ee/startup.sh
+++ ee/startup.sh
@@ -2,26 +2,21 @@
 set -euo pipefail
 echo Running startup script...\n"
 
-install_neo4j_from_yum() {
-  echo "Installing Graph Database..."
-  rpm --import https://debian.neo4j.com/neotechnology.gpg.key
+echo "Installing Graph Database..."
+rpm --import https://debian.neo4j.com/neotechnology.gpg.key
 
-  echo "[neo4j]
+cat <<EOF > /etc/yum.repos.d/neo4j.repo
+[neo4j]
 name=Neo4j RPM Repository
 baseurl=https://yum.neo4j.com/stable/latest
 enabled=1
-gpgcheck=1" > /etc/yum.repos.d/neo4j.repo
+gpgcheck=1
+EOF
 
-  export NEO4J_ACCEPT_LICENSE_AGREEMENT=yes
-  yum -y install neo4j-enterprise
-  systemctl enable neo4j
-}
+export NEO4J_ACCEPT_LICENSE_AGREEMENT=yes
+yum -y install neo4j-enterprise
+systemctl enable neo4j
 
-start_neo4j() {
-  echo "Starting Neo4j..."
-  service neo4j start
-  neo4j-admin dbms set-initial-password "${password}"
-}
-
-install_neo4j_from_yum
-start_neo4j
+echo "Starting Neo4j..."
+service neo4j start
+neo4j-admin dbms set-initial-password "${password}"
