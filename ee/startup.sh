8ed42b6e9809c3dd1743f7de8fc3a5e69b7ba1e6
Ben Lackey <ben.lackey@outlook.com>
2026-02-06 00:15:43 -0800

    using 10

--- ee/startup.sh
+++ ee/startup.sh
@@ -5,11 +5,13 @@ echo Running startup script...\n"
 install_neo4j_from_yum() {
   echo "Installing Graph Database..."
   rpm --import https://debian.neo4j.com/neotechnology.gpg.key
+
   echo "[neo4j]
 name=Neo4j RPM Repository
 baseurl=https://yum.neo4j.com/stable/latest
 enabled=1
 gpgcheck=1" > /etc/yum.repos.d/neo4j.repo
+
   export NEO4J_ACCEPT_LICENSE_AGREEMENT=yes
   yum -y install neo4j-enterprise
   systemctl enable neo4j
