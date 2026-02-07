848daba09152f11ebe2fed44884d784f9a4bb93b
Ben Lackey <ben.lackey@outlook.com>
2026-02-06 18:39:16 -0800

    add make archive

--- marketplace/makeArchive.sh
+++ marketplace/makeArchive.sh
@@ -1,22 +1,15 @@
 #!/bin/sh
 
-rm archive.zip
-mkdir tmp
+for EDITION in "ce" "ee"; do
+  rm $EDITION-archive.zip
+  mkdir tmp
 
-cp neo4j.py tmp
-cp neo4j.py.display tmp
-cp neo4j.py.schema tmp
-cp c2d_deployment_configuration.json tmp
-cp test_config.yaml tmp
+  cp ../$EDITION/* tmp
 
-cp ../simple/deployment.py tmp
-cp ../simple/cluster.py tmp
-cp ../simple/core.sh tmp
-cp ../simple/parseCoreMembers.py tmp
+  cd tmp
 
-cd tmp
-
-zip -r -X archive.zip *
-mv archive.zip ../
-cd ..
-rm -rf tmp
+  zip -r -X $EDITION-archive.zip *
+  mv $EDITION-archive.zip ../
+  cd ..
+  rm -rf tmp
+done
