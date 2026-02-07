25cee0690731eb910381effd16a59b56df827ef5
Ben Lackey <ben.lackey@outlook.com>
2026-02-07 12:50:41 -0800

    upload

--- ee/startup.sh
+++ ee/startup.sh
@@ -1,31 +1,38 @@
 #!/bin/bash
 set -euo pipefail
 
+#!/bin/bash
+set -euo pipefail
+
 echo Running startup script...
 export password="${password}"
 export nodeCount="${nodeCount}"
 export loadBalancerIP="${loadBalancerIP}"
 
-install_neo4j_from_yum() {
-  echo "Installing Graph Database..."
-  rpm --import https://debian.neo4j.com/neotechnology.gpg.key
-  echo "[neo4j]
+echo "Installing Graph Database..."
+rpm --import https://debian.neo4j.com/neotechnology.gpg.key
+echo "[neo4j]
 name=Neo4j RPM Repository
 baseurl=https://yum.neo4j.com/stable/latest
 enabled=1
 gpgcheck=1" > /etc/yum.repos.d/neo4j.repo
-  export NEO4J_ACCEPT_LICENSE_AGREEMENT=yes
-  yum -y install neo4j-enterprise
-  systemctl enable neo4j
-}
+export NEO4J_ACCEPT_LICENSE_AGREEMENT=yes
+yum -y install neo4j-enterprise
 
-extension_config() {
-  echo Configuring extensions and security in neo4j.conf...
-  sed -i s~#server.unmanaged_extension_classes=org.neo4j.examples.server.unmanaged=/examples/unmanaged~server.unmanaged_extension_classes=com.neo4j.bloom.server=/bloom,semantics.extension=/rdf~g /etc/neo4j/neo4j.conf
-  sed -i s/#dbms.security.procedures.unrestricted=my.extensions.example,my.procedures.*/dbms.security.procedures.unrestricted=gds.*,apoc.*,bloom.*/g /etc/neo4j/neo4j.conf
-  echo "dbms.security.http_auth_allowlist=/,/browser.*,/bloom.*" >> /etc/neo4j/neo4j.conf
-  echo "dbms.security.procedures.allowlist=apoc.*,gds.*,bloom.*" >> /etc/neo4j/neo4j.conf
-}
+echo "Configuring network in neo4j.conf..."
+sed -i "s/#server.default_listen_address=0.0.0.0/server.default_listen_address=0.0.0.0/g" /etc/neo4j/neo4j.conf
+
+echo "Starting Neo4j..."
+neo4j-admin dbms set-initial-password "$password"
+systemctl enable neo4j
+/usr/bin/neo4j start
+
+
+
+
+
+
+
 
 build_neo4j_conf_file() {
   echo "Configuring network in neo4j.conf..."
@@ -74,23 +81,3 @@ build_neo4j_conf_file() {
     echo "dbms.cluster.endpoints=$COREMEMBERS" >> /etc/neo4j/neo4j.conf
   fi
 }
-
-add_cypher_ip_blocklist() {
-  echo "internal.dbms.cypher_ip_blocklist=10.0.0.0/8,172.16.0.0/12,192.168.0.0/16,169.254.169.0/24,fc00::/7,fe80::/10,ff00::/8" >> /etc/neo4j/neo4j.conf
-}
-
-start_neo4j() {
-  echo "Starting Neo4j..."
-
-  # service neo4j start
-  # The service wrapper is failing.  Instead, let's try starting directly.
-  /usr/bin/neo4j start
-
-  neo4j-admin dbms set-initial-password "$password"
-}
-
-install_neo4j_from_yum
-extension_config
-build_neo4j_conf_file
-add_cypher_ip_blocklist
-start_neo4j
--- marketplace/README.md
+++ marketplace/README.md
@@ -9,7 +9,9 @@ As an end user, you should have little use for the contents of this directory an
 Google requires completion of an open source worksheet.  Ours is [here](https://docs.google.com/spreadsheets/d/1z2YDbdeUVzHkpEmJGqYfcFHZcSd4rBPazYYH-zSJEg0/edit?usp=sharing).
 
 ## Updating the Listing
-To submit an updated listing, run ./makeArchive.sh.  You'll then need to upload those two archives to the neo4j-terraform-marketplace bucket in neo4j-mp-public.  You can do that with the commands:
+To submit an updated listing, run ./makeArchive.sh.  You'll want to make sure the ce and ee directories don't have TF state files and such in them.
+
+Now you can upload those two archives to the neo4j-terraform-marketplace bucket in neo4j-mp-public.  Run the commands:
 
     gcloud config set project neo4j-mp-public
     gcloud storage cp *.zip gs://neo4j-terraform-marketplace/
